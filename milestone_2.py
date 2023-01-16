6481025a85319eb8276138bb994dfc2a086d5e56
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-16 19:58:49 +0000

    Adds validation for letter input

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,9 @@
 import random
 word_list = ["peach", "strawberry", "raspberry", "pear", "tangerine"]
 word = random.choice(word_list)
-print(word)
+
 guess = input("What letter would you like to guess?")
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
