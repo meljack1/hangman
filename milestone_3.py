47561570904a37fcf160a6e1fb569410383e43b3
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-16 20:10:10 +0000

    Add functionality to choose random word from list

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,7 @@
+import random
+word_list = ["peach", "strawberry", "raspberry", "pear", "tangerine"]
+word = random.choice(word_list)
+
 while True:
     guess = input("What letter would you like to guess?")
     if len(guess) == 1 and guess.isalpha():
