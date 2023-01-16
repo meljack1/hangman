c625cef08b2f40418e76054e82a840e69e2cea49
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-16 19:54:51 +0000

    Asks user for a letter input

--- milestone_2.py
+++ milestone_2.py
@@ -2,3 +2,4 @@ import random
 word_list = ["peach", "strawberry", "raspberry", "pear", "tangerine"]
 word = random.choice(word_list)
 print(word)
+guess = input("What letter would you like to guess?")
