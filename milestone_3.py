513099604b62cdc08e1e80b7c193fdaf565942ec
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-16 20:05:30 +0000

    Adds validation to check letter guessed is a single alphabetical character

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,6 @@
 while True:
     guess = input("What letter would you like to guess?")
-    
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
