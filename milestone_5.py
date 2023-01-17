aef9edcdd21151a4dbd9455c6ee4965e212ce902
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-17 20:33:01 +0000

    Adds break if number of lives is 0 to avoid infinite loop

--- milestone_5.py
+++ milestone_5.py
@@ -39,6 +39,7 @@ def play_game(word_list, num_lives):
     while True:
         if game.num_lives == 0:
             print("You lost!")
+            break
         elif game.num_letters > 0:
             game.ask_for_input()
         else:
