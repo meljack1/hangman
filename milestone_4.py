f57add0dc61203bd9a0c5c7f616e61d6c09eaac6
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-17 20:10:27 +0000

    Adds functionality to deduct life if guess is wrong

--- milestone_4.py
+++ milestone_4.py
@@ -18,7 +18,9 @@ class Hangman():
             self.num_letters -= 1
             print(f"Good guess! {guess} is in the word.")
         else:
+            self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
     def ask_for_input(self):
         while True:
             guess = input("What letter would you like to guess? ")
